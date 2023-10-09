90eb5b0da4b4801e4978073871e6cff20a5c7b91
Igor Ivanov <31894518+IVIGOR13@users.noreply.github.com>
2023-10-09 11:40:16 +0300

    fix

--- task15/main.go
+++ task15/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"strings"
+)
+
 // К каким негативным последствиям может привести данный фрагмент кода, и как
 // это исправить? Приведите корректный пример реализации.
 //
@@ -16,10 +20,22 @@ package main
 var justString string
 
 func someFunc() {
-	// v := createHugeString(1 << 10)
-	// justString = v[:100]
+	str := createHugeString(1 << 10)
 
-	justString = createHugeString(100)
+	strLen := 100
+
+	builder := strings.Builder{}
+	builder.Grow(strLen)
+	for _, v := range []byte(str) {
+		builder.WriteByte(v)
+	}
+	justString = builder.String()
+
+	// justBytes := make([]byte, strLen)
+	// for i, v := range []byte(str) {
+	// 	justBytes[i] = v
+	// }
+	// justString = string(justBytes)
 }
 
 func main() {
